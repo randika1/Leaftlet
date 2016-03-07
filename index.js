2185be7e94bfefe9e50e2109b87ba7f302b6222b
randika1 <rdcsenevirathna@gmail.com>
2016-03-07 11:35:42 +0530

    .gitignore

---
+++ index.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var app = express();
+
+app.get('/maplocation', function(req, res) {
+	res.json({ lat: 89.52, lon: 39.225});
+});
+
+app.listen(3000, function() {
+	console.log('Server running on port 3000');
+});
