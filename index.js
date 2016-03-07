8019b5d1da679517f5558b97be8169b0dc6849aa
randika1 <rdcsenevirathna@gmail.com>
2016-03-07 11:48:59 +0530

    port

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ app.get('/maplocation', function(req, res) {
 	res.json({ lat: 89.52, lon: 39.225});
 });
 
-app.listen(3000, function() {
+app.listen(80, function() {
 	console.log('Server running on port 3000');
 });
