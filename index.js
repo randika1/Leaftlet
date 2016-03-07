81707a068ca07eaa2296f8bb51dfaced49557c73
randika1 <rdcsenevirathna@gmail.com>
2016-03-07 12:11:36 +0530

    root path

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var app = express();
 
+app.get('/', function(req, res) {
+	res.json({ lat: 89.52, lon: 39.225});
+});
+
 app.get('/maplocation', function(req, res) {
 	res.json({ lat: 89.52, lon: 39.225});
 });
